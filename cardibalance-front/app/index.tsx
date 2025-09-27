fe953218fd6f47b67227e6204d58598f2a98944a
ThThiagoth <91805382+ThThiagoth@users.noreply.github.com>
2025-09-27 17:43:39 -0300

    Adicionado tela de Dados do Paciente

--- cardibalance-front/app/index.tsx
+++ cardibalance-front/app/index.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 // import RegisterScreen from './RegisterScreen'
 import HomeScreen from './HomeScreen'
 // import LoginScreen from './LoginPage'
+// import PatientDataScreen from './PatientDataScreen'
 
 export default function Index() {
   return <HomeScreen />
   // return <LoginScreen />
   // return <RegisterScreen />
+  // return <PatientDataScreen />
 }
