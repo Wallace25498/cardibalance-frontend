de20bb3a5f9c1271b56ec957ae40cc0374e89398
ThThiagoth <91805382+ThThiagoth@users.noreply.github.com>
2025-11-14 23:04:38 -0300

    ajuste na tela de dados do paciente

--- cardibalance-front/app/PatientDataScreen.tsx
+++ cardibalance-front/app/PatientDataScreen.tsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Text,
   TextInput,
   TouchableOpacity,
-  View
+  SafeAreaView,
+  View,
+  Pressable,
+  Platform
 } from 'react-native'
 import styles from './styles/PatientDataScreen.styles'
+import DateTimePicker from '@react-native-community/datetimepicker';
+
 
 export default function FeaturesScreen() {
+  {/*date timepicker */}
+  const [date, setDate] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
+  const toggleDatePicker = () => {
+    setShowPicker(!showPicker); }
+  const [dateOfBirth, setDateOfBirth] = useState('');
+  {/*Variavel de armazenamento de genero */}
+  const [selectedGender, setSelectedGender] = useState('');
+
+{/*funções para o date timepicker */}
+  const onChange = (event: any, selectedDate?: Date) => {
+    if (event.type === "set") {
+      const currentDate = selectedDate || date;
+      setDate(currentDate);
+
+      if (Platform.OS === "android") {
+        toggleDatePicker();
+        setDateOfBirth(currentDate.toLocaleDateString());
+      }
+    } else {
+      toggleDatePicker();
+    }
+  };
   return (
     <View style={styles.container}>
       {/* Título */}
@@ -25,30 +53,58 @@ export default function FeaturesScreen() {
         placeholderTextColor="grey"
       />
 
-    {/* opções de Genero */}
-    <View style={styles.gender}> 
-     <TouchableOpacity style={styles.primaryGenderButton}>
-        <Text style={styles.primaryGenderButtonText}>Sexo Masculino</Text>
-      </TouchableOpacity>
+      {/* opções de Genero */}
+      <View style={styles.gender}>
+        <TouchableOpacity
+          style={[
+            styles.secundaryGenderButton,
+            selectedGender === "Masculino" && styles.primaryGenderButton
+          ]}
+          onPress={() => setSelectedGender("Masculino")}
+        >
+          <Text style={[selectedGender === "Masculino" && styles.primaryGenderButtonText]}
+          >Sexo Masculino</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity style={styles.secundaryGenderButton}>
-        <Text style={styles.secundaryGenderButtonText}>Sexo Feminino</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={[
+          styles.secundaryGenderButton,
+          selectedGender === "Feminino" && styles.primaryGenderButton
+        ]}
+          onPress={() => setSelectedGender("Feminino")}>
+          <Text style={[selectedGender === "Feminino" && styles.primaryGenderButtonText]}>Sexo Feminino</Text>
+        </TouchableOpacity>
 
       </View>
 
       {/* Data de Nascimento */}
 
       <View style={styles.date}>
-        <Text style={styles.dateTitle}>Data de Nascimento:</Text>
+        <Text style={styles.dateTitle}>Data de Nascimento:</Text> 
 
+        {!showPicker && (<Pressable onPress={toggleDatePicker}><TextInput
+          value={dateOfBirth}
+          onChangeText={setDateOfBirth}
+          placeholder='dd/mm/aa'
+          style={styles.inputdate}
+          placeholderTextColor="grey"
+          editable={false}
+        /></Pressable>)}
+
+        {showPicker && (
+          <DateTimePicker
+            mode="date"
+            display="spinner"
+            value={date}
+            onChange={onChange}
+          />
+        )}
       </View>
 
       {/* Botão de Confirmar */}
       <TouchableOpacity style={styles.button}>
         <Text style={styles.buttonText}>Confirmar</Text>
       </TouchableOpacity>
-    
+
     </View>
   )
 }
